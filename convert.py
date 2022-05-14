66736db4f7edd6ce1f3525bd4072dca754c5b6b9
Manju <manjuy904@gmail.com>
2022-05-14 12:19:50 +0100

    Initial commit

---
+++ convert.py
@@ -0,0 +1,23 @@
+x = 1 #int
+y = 2.4 #float
+z = 4j #complex
+
+#convert int to float
+a = float(x)
+
+#convert float to int
+b = int(y)
+
+#convert int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+import random
+print(random.randrange(1,10))
---
+++ hello.py
@@ -0,0 +1,55 @@
+print("Hello, Manju")
+if 5 > 2:
+ print("Five is greater than two")
+if 5 > 2: 
+    print("Five is greater than two")
+# if 5 > 2:
+#    print("Five")
+#    print("Five")
+    x = 5
+    y = "Hello Manju" #This is my name
+    print(x)
+    print(y)
+    #This is a comment
+    print("This is me")
+    x = 4
+    print (x)
+    x = "Manju"
+    print(x)
+    z = 2 #z is of type int
+    z = "Manju" #z is now of type str
+    print(z)
+    
+    x = 2
+    y ='Manju'
+    print(type(x))
+    print(type(y))
+    
+    a = 4
+    A = "Manju"
+    print(a)
+    print(A)
+    x, y, z = "Orange","Banana","Cherry"
+    print(x)
+    print(y)
+    print(z)
+    fruits = ["x=apple", "y=banana", "c=cherry"]
+    x, y, z = fruits
+    print(x)
+    print(y)
+    print(z)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
